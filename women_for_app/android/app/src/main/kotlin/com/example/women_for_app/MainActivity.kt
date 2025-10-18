ac51fde0205f5ecad3df86a3fac883746b3ef88b
Hamidullo Ulashov <ulashovhamidullo.com>
2025-10-18 06:07:00 -0700

    uzgarishlar

---
+++ women_for_app/android/app/src/main/kotlin/com/example/women_for_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.women_for_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ women_for_app/lib/main.dart
@@ -0,0 +1,3 @@
+void main(List<String> args) {
+  // zem bosing
+}
